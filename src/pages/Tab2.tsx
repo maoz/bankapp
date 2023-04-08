d62b7f1720c476068b61a8c3a002e04566fe9bbe
Maoz Ziri <maozziri@gmail.com>
2023-04-08 11:52:57 +0300

    dev

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
 
 const Tab2: React.FC = () => {
   return (
@@ -16,7 +22,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Tab 2 page" content={<div>BLA 2</div>} />
       </IonContent>
     </IonPage>
   );
